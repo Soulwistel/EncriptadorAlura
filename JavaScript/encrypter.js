58926d63fb39bb10f086ae9cecaa8cf2589c5cb9
Soul Wistel <emichelle1897@gmail.com>
2023-04-19 15:09:18 -0700

    Terminacion del estilo visual del programa

--- JavaScript/encrypter.js
+++ JavaScript/encrypter.js
@@ -34,8 +34,9 @@ function encriptador(mensaje){
 }
 
 function desencriptador(mensaje){
-    const uppercase= ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+    const uppercase= ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
     const lowercase= uppercase.map(v=> v.toLowerCase());
+    const number= [0,1,2,3,4,5,6,7,8,9];
     const splitString = mensaje.split("");
     var text;
     for(var i=0;i<splitString.length;i++){
