07aa0884d0ea2a3e0b57792e16ca3f9e9f0cb9f3
Soul Wistel <emichelle1897@gmail.com>
2023-04-19 17:50:39 -0700

    caractares numeriscos y especiales agregados

--- JavaScript/encrypter.js
+++ JavaScript/encrypter.js
@@ -1,12 +1,16 @@
 
 function encriptador(mensaje){
-    const uppercase= ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+    const uppercase= ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
     const lowercase= uppercase.map(v=> v.toLowerCase());
+    const number= ["0","1","2","3","4","5","6","7","8","9"];
+    const especial=["`","~","!","@","#","$","%","^","&","*","(",")","-","_","=","+"]
     const splitString = mensaje.split("");
     var text;
     for(var i=0;i<splitString.length;i++){
         if(splitString[i]==" "){
-            splitString[i]='@';
+            splitString[i]='▓';
+        }else if(splitString[i]=="▓"){
+            splitString[i]=' ';
         }else if(splitString[i]==splitString[i].toUpperCase()){
             for(var j=0;j<=uppercase.length;j++){
                 if(splitString[i]==uppercase[j] && j%2==0){
@@ -15,6 +19,22 @@ function encriptador(mensaje){
                 }else if(splitString[i]==uppercase[j]){
                     splitString[i]=uppercase[j-1];
                     break;
+                }else if(splitString[i]==number[j]){
+                    if(j%2==0){
+                        splitString[i]=number[j+1];
+                        break;
+                    }else{
+                        splitString[i]=number[j-1];
+                        break;
+                    }
+                }else if(splitString[i]==especial[j]){
+                    if(j%2==0){
+                        splitString[i]=especial[j+1];
+                        break;
+                    }else{
+                        splitString[i]=especial[j-1];
+                        break;
+                    }
                 }
             }
         }else if(splitString[i]==splitString[i].toLowerCase()){
@@ -37,10 +57,11 @@ function desencriptador(mensaje){
     const uppercase= ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
     const lowercase= uppercase.map(v=> v.toLowerCase());
     const number= [0,1,2,3,4,5,6,7,8,9];
+    const especial=["`","~","!","@","#","$","%","^","&","*","(",")","-","_","=","+"];
     const splitString = mensaje.split("");
     var text;
     for(var i=0;i<splitString.length;i++){
-        if(splitString[i]=="@"){
+        if(splitString[i]=="▓"){
             splitString[i]=' ';
         }else if(splitString[i]==splitString[i].toUpperCase()){
             for(var j=0;j<=uppercase.length;j++){
@@ -50,6 +71,22 @@ function desencriptador(mensaje){
                 }else if(splitString[i]==uppercase[j]){
                     splitString[i]=uppercase[j+1];
                     break;
+                }else if(splitString[i]==number[j]){
+                    if(j%2==0){
+                        splitString[i]=number[j+1];
+                        break;
+                    }else{
+                        splitString[i]=number[j-1];
+                        break;
+                    }
+                }else if(splitString[i]==especial[j]){
+                    if(j%2==0){
+                        splitString[i]=especial[j+1];
+                        break;
+                    }else{
+                        splitString[i]=especial[j-1];
+                        break;
+                    }
                 }
             }
         }else if(splitString[i]==splitString[i].toLowerCase()){
